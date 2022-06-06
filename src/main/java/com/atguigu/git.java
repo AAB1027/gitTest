7f4f25068abc75de8375878bb590f7e5c9d3bed1
xpp <xpp@atguigu.com>
2022-06-06 23:37:32 +0800

    first commit

---
+++ src/main/java/com/atguigu/git.java
@@ -0,0 +1,15 @@
+package com.atguigu;
+
+import org.junit.Test;
+
+/**
+ * @author xpp
+ * @create 2022-06-06 23:26
+ */
+public class git {
+    @Test
+    public void test(){
+        System.out.println("hello.tt!");
+    }
+
+}
---
+++ src/main/java/com/atguigu/test.java
@@ -0,0 +1,8 @@
+package com.atguigu;
+
+/**
+ * @author xpp
+ * @create 2022-06-06 23:31
+ */
+public class test {
+}
